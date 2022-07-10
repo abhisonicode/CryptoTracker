cd68d1f201bf7e8117a90b8ea3fefeff9c2676f8
abhisonicode <92227490+abhisonicode@users.noreply.github.com>
2022-07-10 23:12:22 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              KrypTracker
+              Crypto Tracker
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
